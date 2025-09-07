98e7216fae7bd1cc2a6716c7f1c6f06e2ad067f3
Muhammad Muzammil <m.muzammilnt@gmail.com>
2025-09-07 20:40:47 +0800

    Update upload_to_anki.py

--- upload_to_anki.py
+++ upload_to_anki.py
@@ -1,1 +1,53 @@
-to anki
+import requests
+import socket
+
+
+URL = "http://localhost:8765/"
+
+def upload_anki(word,definition,card_type,mp3=None):
+
+    """
+        upload a single note to Anki via AnkiConnect.
+        Default deck name is : vocabulary-auto
+    """
+
+    payload = {
+        "action": "addNotes",
+        "version": 5,
+        "params": {
+            "notes": [
+                {
+                    "deckName": "vocabulary-auto",
+                    "modelName": card_type,
+                    "fields": {
+                        "Front": word,
+                        "Back": definition
+                    },
+                    "tags": [
+                        "automate"
+                    ],
+                    "audio": [{
+                        "url" : mp3,
+                        "fields" : ["Front"],
+                        "filename": f"{word}.mp3",
+                        "skipHash": "7e2c2f954ef6051373ba916f000168dc"
+
+                    }]
+
+
+                }
+            ]
+        }
+    }
+
+    requests.post(URL,json=payload)
+
+# check if AnkiConnect is running and listening
+def is_anki_listening(host="127.0.0.1", port =8765,timeout= 1):
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
+        sock.settimeout(timeout)
+        try:
+            sock.connect((host,port))
+            return True
+        except (ConnectionRefusedError,socket.timeout):
+            return False
